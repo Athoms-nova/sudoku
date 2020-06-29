ec4900bb3eb911cc497f9626c50055b5456bf9d3
Athoms-nova <lova.raheri@gmail.com>
2020-06-30 00:41:15 +0200

    Updates

---
+++ precache-manifest.07e3388a30a94a7df5f9c32e16ed5695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4951216d9c6db8d2ab79edc472078e4",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "12f2abc80119dccd3fe1",
+    "url": "/sudoku/static/css/main.b016ebc1.chunk.css"
+  },
+  {
+    "revision": "b6f2c886b5a9612f0f2d",
+    "url": "/sudoku/static/js/2.c7db5a1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sudoku/static/js/2.c7db5a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f2abc80119dccd3fe1",
+    "url": "/sudoku/static/js/main.1179a6c2.chunk.js"
+  },
+  {
+    "revision": "5e467a82d5680057536a",
+    "url": "/sudoku/static/js/runtime-main.e006c59b.js"
+  }
+]);
